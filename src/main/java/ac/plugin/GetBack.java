ba0f4e57b3ffd65f3f7ab755526b72950420cf6d
alessandro <campoloalex@gmail.com>
2022-02-26 01:40:27 +0100

    Fixed a bug in deaths save/load that broke config.yml loading when players had special characters in their name Updated to version 1.2.0

--- src/main/java/ac/plugin/GetBack.java
+++ src/main/java/ac/plugin/GetBack.java
@@ -1,7 +1,9 @@
 package ac.plugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,11 +14,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.NumberConversions;
 
 import javax.annotation.Nonnull;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class GetBack extends JavaPlugin implements Listener, CommandExecutor {
 
@@ -36,7 +37,7 @@ public class GetBack extends JavaPlugin implements Listener, CommandExecutor {
             saveConfig();
         }
         if (!config.contains("deaths")) {
-            config.set("deaths", deaths);
+            config.set("deaths", new ArrayList<>());
             saveConfig();
         } else {
             // Load deaths 1 tick after the server finished startup
@@ -44,8 +45,7 @@ public class GetBack extends JavaPlugin implements Listener, CommandExecutor {
             new BukkitRunnable() {
                 @Override
                 public void run() {
-                    Objects.requireNonNull(getConfig().getConfigurationSection("deaths")).getValues(true)
-                            .forEach((key, value) -> deaths.put(key, (Location) value));
+                    deaths = unpackMap(getConfig().getMapList("deaths"));
                     getServer().broadcastMessage("[GetBack] Loaded " + deaths.size() + " death location(s)");
                 }
             }.runTaskLater(this, 1L);
@@ -58,15 +58,71 @@ public class GetBack extends JavaPlugin implements Listener, CommandExecutor {
         getLogger().info("GetBack disabled");
     }
 
+    /**
+     * Serialize deaths map as a list, adding player name as one of the values of the map
+     * @param map A String-Location map containing the death location for each player
+     * @return A list of Location maps
+     */
+    public List<Map<String, Object>> packMap(Map<String, Location> map){
+        List<Map<String, Object>> out = new ArrayList<>();
+        map.forEach((s, location) -> {
+            Map<String, Object> temp = location.serialize();
+            // add player name to location map and add it to the list
+            temp.put("player", s);
+            out.add(temp);
+        });
+        return out;
+    }
+
+    /**
+     * Deserialize deaths map as a list
+     * @param list A list of Location maps
+     * @return A String-Location map containing the death location for each player
+     */
+    public Map<String, Location> unpackMap(List<Map<?, ?>> list){
+        Map<String, Location> out = new HashMap<>();
+        list.forEach(args -> {
+            /*
+            Rewrote Bukkit Location deserialize method
+            Find world by its name as in original method
+             */
+            World world = null;
+            if (args.containsKey("world")) {
+                world = Bukkit.getWorld((String)args.get("world"));
+                if (world == null) {
+                    throw new IllegalArgumentException("unknown world");
+                }
+            }
+            // Add elements to new output map and prepare Location object
+            out.put((String)args.get("player"), new Location(
+                    world,
+                    NumberConversions.toDouble(args.get("x")),
+                    NumberConversions.toDouble(args.get("y")),
+                    NumberConversions.toDouble(args.get("z")),
+                    NumberConversions.toFloat(args.get("yaw")),
+                    NumberConversions.toFloat(args.get("pitch"))
+            ));
+
+        });
+        return out;
+    }
+
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event){
         deaths.put(event.getEntity().getDisplayName(), event.getEntity().getLocation());
-        config.set("deaths", deaths);
+        /*
+        we need to save locations as maps with player name as one of its values,
+        since native saving method uses player name as a key, messing up the configuration
+        when players have special characters in their name
+         */
+        config.set("deaths", packMap(deaths));
         saveConfig(); // saving config here just in case the plugin would crash before onDisable is called
         event.getEntity().sendMessage("" /*removes NPE warning*/ + config.getString("deathmessage"));
-        // IDE warns about config.getString() returning null,
-        // virtually impossible since its value being present
-        // is checked when loading config in onEnable
+        /*
+        IDE warns about config.getString() returning null,
+        virtually impossible since its value being present
+        is checked when loading config in onEnable
+         */
     }
 
     @Override
